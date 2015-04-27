25c74fcba137c7522361e8f58591a8038e0ccbef
Jacob Carey <jacobcvt12@gmail.com>
2015-04-27 16:30:15 -0400

    Add old parameters

---
+++ R/old_model.R
@@ -0,0 +1,16 @@
+# parameters
+a.r <- 0.75
+a.s <- 0.22
+a.cn <- a.cp <- 0.01
+a.d <- 0.01
+alpha <- 0.5
+theta <- 0.033
+beta.c <- beta.d <- 0.007
+f <- 0.6
+epsilon <- 0.1
+p <- 0.8
+phi <- 0.2
+k.r <- 0.33
+k <- 0.15
+k.d <- 0.068
+
