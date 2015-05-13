9b06631e04623b88e6713081cfb5601ebc9cd0c0
apyden <apyden@gmail.com>
2015-05-13 17:22:03 -0400

    deleted admit rate for S.ft corrected discharge rates

--- R/model.R
+++ R/model.R
@@ -75,11 +75,11 @@ dx.dt.new <- function(t, y, param, delta = 1e-1) {
     N = R + S.abx + S.ft + C + D
 
     # ODEs
-    dR <- a.r * delta*N + theta.abx*S.abx + theta.ft * S.ft - k.r*R - alpha * R
-    dS.ft <- a.s.ft * delta * N + rho * e * D * tau - 
+    dR <- a.r * delta*N + theta.abx*S.abx + theta.ft * S.ft - k.r * R - alpha * R
+    dS.ft <- rho * e * D * tau - 
            theta.ft * S.ft - k * S.ft - lambda * S.ft
     dS.abx <- a.s.abx * delta * N + alpha * R + p * epsilon * D * (1-tau) - 
-           theta.abx * S.abx - k * S - lambda * S.abx
+           theta.abx * S.abx - k * S.abx - lambda * S.abx
     dC <- a.c * delta * N + lambda * S.abx + lambda * S.ft - phi * C - k * C
     dD <- a.d * delta * N + phi * C - p * epsilon * D * (1-tau) - rho * e * D * tau - k.d * D
 
